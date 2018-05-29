9e0919400e5779962eebd9fecc71856306dd241a
Danex2 <khadane.miller@gmail.com>
2018-05-29 18:53:25 -0400

    fixed roster / commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     const embed = new Discord.RichEmbed()
-    .setColor("#b34d4d")
-    .addField("Current commands", "prefix, prog, roster, char, mythic, patch, sched")
-    message.channel.send({embed});
+        .setColor("#b34d4d")
+        .addField("Current commands", "prefix [new prefix here]\nroster (displays current raid roster)\nchar [server] [character]\nmythic (displays mythic prefixes)\npatch (current patch)\nsched [day] [short message]\n")
+    message.channel.send({ embed });
 }
 
 module.exports.help = {
--- commands/roster.js
+++ commands/roster.js
@@ -1,7 +1,20 @@
 const Discord = require("discord.js");
+const roster = require('./../roster.json');
+const fs = require('fs');
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send("heyo")
+    if (args[0] === 'add') {
+        [arg, role, ...args] = [...args];
+        roster[role] = args
+        fs.writeFile("./roster.json", JSON.stringify(roster), (err) => console.error);
+    } else if (args[0] === 'show') {
+        embed = new Discord.RichEmbed()
+            .setColor("#b34d4d")
+            .addField("Tanks", roster['tank'].join(' / '))
+            .addField("Healers", roster['healer'].join(' / '))
+            .addField("DPS", roster['dps'].join(' / '))
+        message.channel.send({ embed });
+    }
 }
 
 module.exports.help = {
