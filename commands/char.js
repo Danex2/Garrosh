d9527727f24241298e051ea6e75094add3836971
Danex2 <khadane.miller@gmail.com>
2018-05-29 18:59:19 -0400

    i love es6 lol

--- commands/char.js
+++ commands/char.js
@@ -5,32 +5,31 @@ const classes = require("./../class.json");
 const blizzard = require('blizzard.js').initialize({ apikey: botSettings.blizzKey });
 
 module.exports.run = async (bot, message, args) => {
-  let server = args[0];
-  let char = args[1];
+  let [server, char] = [...args]
   blizzard.wow.character(['items'], { origin: 'us', realm: server, name: char })
     .then(response => {
       const embed = new Discord.RichEmbed()
-      .setColor("#b34d4d")
-      .setAuthor(`${response.data['name']}\'s profile`)
-      .addField("Character name", response.data['name'], true)
-      .addField("Server", response.data['realm'], true)
-      .addField("Level/Race/Class", response.data['level'] + " " + races[response.data['race']] + " " + classes[response.data['class']], true)
-      .addField("Item Level", response.data['items']['averageItemLevelEquipped'], true)
-      message.channel.send({embed});
+        .setColor("#b34d4d")
+        .setAuthor(`${response.data['name']}\'s profile`)
+        .addField("Character name", response.data['name'], true)
+        .addField("Server", response.data['realm'], true)
+        .addField("Level/Race/Class", response.data['level'] + " " + races[response.data['race']] + " " + classes[response.data['class']], true)
+        .addField("Item Level", response.data['items']['averageItemLevelEquipped'], true)
+      message.channel.send({ embed });
       //console.log(response.data);
-}).catch(function(err) {
-  if (err) {
-     const embed = new Discord.RichEmbed()
-    .setColor("#b34d4d")
-    .setAuthor("404")
-    .addField("Character not found", "Maybe a typo?")
-    message.channel.send({embed});
-  }
-});
+    }).catch(function (err) {
+      if (err) {
+        const embed = new Discord.RichEmbed()
+          .setColor("#b34d4d")
+          .setAuthor("404")
+          .addField("Character not found", "Maybe a typo?")
+        message.channel.send({ embed });
+      }
+    });
 }
 
 module.exports.help = {
-    name: "char"
+  name: "char"
 }
 
 //this command will return basic info about a player's character / could include raid progression stats
